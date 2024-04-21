8c66702c96c5209e603f17c5dc9b7074de1b9cf5
emaniwilli <116006194+emaniwilli@users.noreply.github.com>
2024-04-21 00:19:21 -0400

    Delete Service directory

--- Service/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const app = require('./app');
-const http = require('http');
-const server = http.createServer(app)
-
-
-app.listen(3000, () => console.log('Server Started'))
-
